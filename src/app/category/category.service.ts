3c0c8fef7574034817fe3bc56ed824ac1f87eb5e
Laura-git-hub <laura.baeza.ruiz@gmail.com>
2025-05-26 17:22:49 +0200

    Obteniendo datos de springboot

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,23 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Category } from './model/Category';
-import { CATEGORY_DATA } from './model/mock-categories';
 
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
-export class CategoryService {
+export class CategoryService { 
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  getCategories(): Observable<Category[]> {
-    return of(CATEGORY_DATA);
-  }
-  saveCategory(category: Category): Observable<Category> {
-    return of(null);
-  }
+    private baseUrl = 'http://localhost:8080/category';
 
-  deleteCategory(idCategory : number): Observable<any> {
-    return of(null);
-  }
+    getCategories(): Observable<Category[]> {
+        return this.http.get<Category[]>(this.baseUrl);
+    }
+
+    saveCategory(category: Category): Observable<Category> {
+        //return of(null);
+        const { id } = category;
+        const url = id ? `${this.baseUrl}/${id}` : this.baseUrl;
+        return this.http.put<Category>(url, category);
+    }
+
+    deleteCategory(idCategory : number): Observable<any> {
+        //return of(null);
+        return this.http.delete(`${this.baseUrl}/${idCategory}`);
+    }  
 }
