2b07f28b79c967f404587531e38fe6672146896a
Laura-git-hub <laura.baeza.ruiz@gmail.com>
2025-05-28 11:37:53 +0200

    Actualizando listado Paginado

---
+++ src/app/author/model/AuthorPage.ts
@@ -0,0 +1,10 @@
+import { Pageable } from  '../../core/model/page/Pageable';
+import { Author } from "./Author";
+
+export class AuthorPage {
+    content: Author[];
+    pageable: Pageable;
+    totalElements: number;
+}
+
+export { Pageable };
