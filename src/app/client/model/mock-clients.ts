cd482e36d274398e8178ce11871e12ab9d869feb
Laura-git-hub <laura.baeza.ruiz@gmail.com>
2025-05-29 13:56:45 +0200

    Cliente Editado

---
+++ src/app/client/model/mock-clients.ts
@@ -0,0 +1,11 @@
+import { Client } from "./Client";
+
+export const CLIENT_DATA: Client[] = [
+    { id: 1, name: 'Alberto' },
+    { id: 2, name: 'Sara' },
+    { id: 3, name: 'Isabel' },
+    { id: 4, name: 'Sofia' },
+    { id: 5, name: 'Juan' },
+    { id: 6, name: 'Amaia' },
+    { id: 7, name: 'Iris' },    
+]   
